abcfb876322dba7551a16718084df5d21640e1a8
Yusif Huseynov <hv.yusif@mail.ru>
2018-12-31 23:29:35 +0400

    Initial Commit

---
+++ Simpson Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpson Book
+//
+//  Created by Yusif Huseynov on 12/31/18.
+//  Copyright © 2018 Yusif Huseynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
